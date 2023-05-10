7266011146ffdad3a218bac872eb1adcecda73c0
eder0782 <eder0782@gmail.com>
2023-05-10 11:46:08 -0400

    inicio

---
+++ bin/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const teste_1 = require("./teste");
+console.log((0, teste_1.hello)('Eder'));
---
+++ index.ts
@@ -0,0 +1,5 @@
+import { hello } from "./teste";
+
+
+
+console.log(hello('Eder'));
