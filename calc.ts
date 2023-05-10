b138cbd62907d3cc616500ef0faac27da5d5bd20
eder0782 <eder0782@gmail.com>
2023-05-10 13:21:18 -0400

    update type calcModel

--- calc.ts
+++ calc.ts
@@ -1,11 +1,14 @@
 type calcModel={
     valor1:number,
-    valor2:number
+    valor2:number,
+    total: number
+
 }
 
 const calcula:calcModel={
     valor1:0,
     valor2:0,
+    total:0,
 }
 
 export default calcula;
